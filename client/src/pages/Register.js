1d993d8eb81a11b758b182af8f296fed02febf22
Sudeep Dhakal_CP390 <sdhakal@confederationcollege.ca>
2024-03-14 19:14:24 -0400

    Installed react router dom and ant design. Setup link path in App.js file to homepage, login and register page, created and styled register and login page respectively, installed and configured concurrently package in package.json file to run client and server at the same time because they are in differnet ports.

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import "../styles/Register.css"
+import { Form, Input } from "antd";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+
+
+    const onfinishHandler = async (values) => {
+        console.log(values)
+    }
+
   return (
-    <div>
-        <h1>Register</h1>
-    </div>
+    <>
+        <div className="form-container ">
+            <Form
+                layout="vertical"
+                onFinish={onfinishHandler}
+                className="register-form"
+            >
+            <h3 className="text-center">Register</h3>
+            <Form.Item label="Name" name="name">
+                <Input type="text" required />
+            </Form.Item>
+            <Form.Item label="Email" name="email">
+                <Input type="email" required />
+            </Form.Item>
+            <Form.Item label="Password" name="password">
+                <Input type="password" required />
+            </Form.Item>
+            <Link to="/login" className="m-2">
+                Have an account? Login Here.
+            </Link>
+            <button className="btn btn-primary" type="submit">
+                Register
+            </button>
+            </Form>
+        </div>
+    </>
   )
 }
 
