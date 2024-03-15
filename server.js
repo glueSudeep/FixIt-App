aafee938560a7349aea36df65e0a76683542d3e1
Sudeep Dhakal_CP390 <sdhakal@confederationcollege.ca>
2024-03-14 20:16:17 -0400

    Connected the serve to MongoDB, created user model, user controller and defined user route and used the route in the server.js file.

--- server.js
+++ server.js
@@ -2,21 +2,32 @@ const express = require("express");
 const colors = require("colors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const router = require("./routes/userRoutes");
 
-// rest object
+// express app
 const app = express();
 
 // .env config 
 dotenv.config();
 
+// Connect to Database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
+  } catch (error) {
+    console.log(`Mongodb Server Issue ${error}`.bgRed.white);
+  }
+};
+connectDB();
+
+//middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get('/', (res,req) => {
-    res.status(200).send({
-        message: "server running",
-    })
-})
+// routes
+app.use("/api/v1/user", router);
 
 const port = process.env.PORT || 8080;
 //listen port
